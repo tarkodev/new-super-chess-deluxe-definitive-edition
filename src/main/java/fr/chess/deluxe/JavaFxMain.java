c2e91e9ff859f7d71d8e781e539ad7f63715e913
Tarkod <mouradgabriel9@gmail.com>
2023-05-06 13:50:06 +0200

    add a way to compile

---
+++ src/main/java/fr/chess/deluxe/JavaFxMain.java
@@ -0,0 +1,8 @@
+package fr.chess.deluxe;
+
+public class JavaFxMain {
+
+    public static void main(String[] args) {
+        ChessMain.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,5 @@ module fr.chess.deluxe {
     requires javafx.fxml;
     requires com.google.gson;
 
-
-    opens fr.chess.deluxe to javafx.fxml, com.google.gson;
     exports fr.chess.deluxe;
-    exports fr.chess.deluxe.piece;
-    opens fr.chess.deluxe.piece to javafx.fxml, com.google.gson;
-    exports fr.chess.deluxe.utils;
-    opens fr.chess.deluxe.utils to javafx.fxml, com.google.gson;
-	exports fr.chess.deluxe.movement;
-	opens fr.chess.deluxe.movement to javafx.fxml, com.google.gson;
 }
